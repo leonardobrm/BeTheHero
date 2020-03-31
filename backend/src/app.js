89a617dd0b956a4781b3c7bf5793c6a45e759b0b
Leonardo Brasil <leonardo.brasil59@outlook.com>
2020-03-31 17:25:38 -0300

    :bug: withdrawing the App class because of a bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,26 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
-class App {
-  constructor() {
-    this.server = express();
+const server = express();
 
-    this.middlewares();
-    this.routes();
-    this.cors();
-  }
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
-  middlewares() {
-    this.server.use(express.json());
-  }
-
-  routes() {
-    this.server.use(routes);
-  }
-
-  cors() {
-    this.server.use(cors());
-  }
-}
-
-export default new App().server;
+export default server;
